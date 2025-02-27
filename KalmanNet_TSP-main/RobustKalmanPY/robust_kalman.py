9eac36bb1b1682fc5cd39161a75ded578c403ef2
RiccardoZattra <riccardo.zattra19@gmail.com>
2025-02-27 11:34:05 +0100

    Verision without initialization of the vectors

--- KalmanNet_TSP-main/RobustKalmanPY/robust_kalman.py
+++ KalmanNet_TSP-main/RobustKalmanPY/robust_kalman.py
@@ -29,12 +29,28 @@ class RobustKalman():
         self.p = torch.Tensor.numpy(self.R).shape[0] #output dimension
         
         self.Xrekf = torch.zeros(self.n, self.T+1) #allocation of memory to save \hat x_t
-        self.Xrekf[:, 0] = self.x0
+        #self.Xrekf[:, 0] = self.x0
+        self.Xrekf_prev = self.x0.squeeze(0)
+        #print(self.x0.shape)
+        #print(self.x0)
+        #print(self.Xrekf[:, 0].shape)
+        #print(self.Xrekf[:, 0])
+        #print(self.Xrekf_prev.shape)
+        #print(self.Xrekf_prev)
+        #print("--------------------------")
         
         self.Xn = torch.zeros(self.n, self.T) #allocation of memory to save \hat x_t|t
         
         self.V = torch.zeros(self.n, self.n, self.T+1) #allocation of memory to save V_t
-        self.V[:,:,0] = 1e-3*torch.eye(2, 2)
+        self.V_prev = 1e-3*torch.eye(2, 2)
+        #self.V[:,:,0] = 1e-3*torch.eye(2, 2)
+        #print((1e-3*torch.eye(2, 2)).shape)
+        #print(1e-3*torch.eye(2, 2))
+        #print(self.V[:,:,0].shape)
+        #print(self.V[:,:,0])
+        #print(self.V_prev.shape)
+        #print(self.V_prev)
+        #print("--------------------------")
         
         self.A = torch.zeros(self.n, self.n, self.T) #allocation of memory to save the linearizatio of state equation
         self.C = torch.zeros(self.p, self.n, self.T) #allocation of memory to save the linearizatio of output equation
@@ -56,6 +72,7 @@ class RobustKalman():
             h_jac = torch.tensor([[1-2*x_rekf_temp[0], 2*x_rekf_temp[1]-1]])
         else:
             h_jac = torch.autograd.functional.jacobian(self.model.h,x_rekf_temp)
+            
         return h_jac
     
     def fnComputeTheta(self, P_pred):
@@ -76,13 +93,15 @@ class RobustKalman():
                 t1 = theta
         
         return theta
-    
+    """
     # Computation of the REKF
     def fnREKF(self):
         for i in range(0, self.T):
+            print(self.Xrekf[:,i].shape)
+            print(self.Xrekf[:,i])
             # C_t
             self.C[:, :, i] = self.fnComputeJacobianH(self.Xrekf[:,i])
-            
+            input();
             # L_t
             L = self.V[:,:,i] @ torch.transpose(self.C[:,:,i], 0, 1) @ torch.linalg.solve(self.C[:,:,i] @ self.V[:, :, i] @ torch.transpose(self.C[:,:,i], 0, 1) + self.R,torch.eye(self.p))
             
@@ -111,9 +130,44 @@ class RobustKalman():
             self.V[:, :, i+1] = torch.linalg.solve(torch.linalg.solve(P,torch.eye(self.n)) - self.th[i] * torch.eye(self.n),torch.eye(self.n))
             
         return [self.Xrekf, self.V]
+    """
     
+    # Computation of the REKF
+    def fnREKF(self):
+        for i in range(0, self.T):
+            
+            # C_t
+            self.C[:, :, i] = self.fnComputeJacobianH(self.Xrekf_prev)
+            
+            # L_t
+            L = self.V_prev @ torch.transpose(self.C[:,:,i], 0, 1) @ torch.linalg.solve(self.C[:,:,i] @ self.V_prev @ torch.transpose(self.C[:,:,i], 0, 1) + self.R,torch.eye(self.p))
+            
+            # h(\hat x_t)
+            hn = self.model.h(self.Xrekf_prev)
+            
+            # \hat x_t|t
+            self.Xn[:, i] = self.Xrekf_prev + torch.squeeze(L * (self.y[:,i] - hn))
+            
+            # A_t
+            self.A[:, :, i] = self.fnComputeJacobianF(self.Xn[:,i])
+            
+            # G_t
+            self.G[:, :, i] = self.A[:, :, i] @ L
+            
+            # \hat x_t+1
+            self.Xrekf[:, i+1] = torch.squeeze(self.model.f(self.Xn[:, i]))
+            self.Xrekf_prev = self.Xrekf[:, i+1]
+            
+            # P_t+1 - The massive fucking riccatti equation
+            P = self.A[:, :, i] @ self.V_prev @ torch.transpose(self.A[:, :, i], 0, 1) - self.A[:, :, i] @ self.V_prev @ torch.transpose(self.C[:, :, i], 0, 1) @ torch.linalg.solve(self.C[:, :, i] @ self.V_prev @ torch.transpose(self.C[:, :, i], 0, 1) + self.R,torch.eye(self.p)) @ self.C[:, :, i] @ self.V_prev @ torch.transpose(self.A[:, :, i], 0, 1) + self.Q
 
-
-
+            # th_t
+            self.th[i] = self.fnComputeTheta(P)
+            
+            # V_t+1
+            self.V[:, :, i+1] = torch.linalg.solve(torch.linalg.solve(P,torch.eye(self.n)) - self.th[i] * torch.eye(self.n),torch.eye(self.n))
+            self.V_prev = self.V[:, :, i+1]
+            
+        return [self.Xrekf, self.V]     
 
 
